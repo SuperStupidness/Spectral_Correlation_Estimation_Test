d8dd9daef2a01010720653703b2695f4d73d8dfa
SuperStupidness <sonymh755@gmail.com>
2026-02-02 10:51:39 +1100

    Update gitignore again

--- scf_test/core/__init__.py
+++
@@ -1,7 +0,0 @@
-# cel_signal_gen_lib/visualization/__init__.py
-"""
-Pulse shaping design tools for communications signals, including Rectangular,
-Square Root Raised Cosine, and Gaussian filters.
-"""
-
-from .filter_design import srrc_design, gaussian_design, rectangular_design
